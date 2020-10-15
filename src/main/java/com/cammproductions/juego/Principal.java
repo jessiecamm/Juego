621c7d9c4d1ce9d4808b3058046906c4dd14ef5f
Jessie <Jessie@Camm>
2020-10-15 12:24:00 -0300

    Prueba nuevo héroe-villano unsopported

--- src/main/java/com/cammproductions/juego/Principal.java
+++ src/main/java/com/cammproductions/juego/Principal.java
@@ -26,13 +26,13 @@ public class Principal {
         h1.setEnergia(68);
         h1.setHabilidad_ppal("Super Puñetazo");
         h1.setIndice_bondad(10);
-
-        Heroes h2=new Heroes(72, "Llamarada", "Sudafrica", "Bola de Fuego", 8);
         
+              
         System.out.println("El héroe " + h1.getNombre() + " de origen " + h1.getPais_origen() + 
                 " tiene una energía de " + h1.getEnergia() + " y su principal habilidad es el " +
                 h1.getHabilidad_ppal() + ".");
         System.out.println();
+        
         
         Villanos v1=new Villanos();
         v1.setNombre("Cruela");
@@ -41,17 +41,11 @@ public class Principal {
         v1.setHabilidad_ppal("Vuela muy alto");
         v1.setIndice_maldad(10);
         
-          System.out.println("El villano " + v1.getNombre() + " de origen " + v1.getPais_origen() + 
+        System.out.println("El villano " + v1.getNombre() + " de origen " + v1.getPais_origen() + 
                 " tiene una energía de " + v1.getEnergia() + " y su principal habilidad es que " +
                 v1.getHabilidad_ppal() + ".");
         System.out.println();
         
-        System.out.println("Aguante este equipo!!!\n");
-        System.out.println("Como verás, pude instalar y hacer mi branch, todo desde net!!!");
-        
-        System.out.println(h2.toString());
     }
-    
-     
     
 }
