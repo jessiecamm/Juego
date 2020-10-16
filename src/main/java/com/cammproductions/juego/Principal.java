275878b69dbbad1000a41c3db20591803bd11023
Jessie <Jessie@Camm>
2020-10-16 12:42:28 -0300

    Energia como constante sin getters y setters

--- src/main/java/com/cammproductions/juego/Principal.java
+++ src/main/java/com/cammproductions/juego/Principal.java
@@ -29,8 +29,7 @@ public class Principal {
         Heroes h2=new Heroes(100,8, "Halcón del desierto","Nueva Zelanda", "Ojos láser");
               
         System.out.println("El héroe " + h1.getNombre() + " de origen " + h1.getPais_origen() + 
-                " tiene una energía de " + h1.getEnergia() + " y su principal habilidad es el " +
-                h1.getHabilidad_ppal() + ".");
+                " y su principal habilidad es el " + h1.getHabilidad_ppal() + ".");
         System.out.println();
         
         System.out.println(h2.toString());
@@ -41,18 +40,16 @@ public class Principal {
         v1.setHabilidad_ppal("Vuela muy alto");
         v1.setIndice_maldad(10);
         
-        Villanos v2=new Villanos(100,8, "Destructor", "Nueva York", "Fuerza extrema");
-                
+        Villanos v2=new Villanos(100, 8, "Destructor", "Nueva York", "Fuerza extrema");
+        
+        System.out.println();        
         System.out.println("El villano " + v1.getNombre() + " de origen " + v1.getPais_origen() + 
-                " tiene una energía de " + v1.getEnergia() + " y su principal habilidad es que " +
-                v1.getHabilidad_ppal() + ".");
+                " y su principal habilidad es que " + v1.getHabilidad_ppal() + ".");
         System.out.println();
         
         System.out.println("El villano " + v2.getNombre() + " de origen " + v2.getPais_origen() + 
-                " tiene una energía de " + v2.getEnergia() + " y su principal habilidad es que " +
-                v2.getHabilidad_ppal() + ".");
+                " y su principal habilidad es que " + v2.getHabilidad_ppal() + ".");
         System.out.println();
         
-    }
-    
+    }   
 }
