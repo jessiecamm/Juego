dc6bd9880f190c0ee257e15ff73e35b1e850437a
Jessie <Jessie@Camm>
2020-10-22 10:30:40 -0300

    Borrar lineas Lau dijo sobraban

--- src/main/java/com/cammproductions/juego/Villanos.java
+++ src/main/java/com/cammproductions/juego/Villanos.java
@@ -54,8 +54,7 @@ public class Villanos extends Super_X {
     }
 
     void debilitar(int atacarHeroe_1) {
-        throw new UnsupportedOperationException("Not supported yet."); 
-    //To change body of generated methods, choose Tools | Templates.
+       
     }
     
 }
